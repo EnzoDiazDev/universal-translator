de06d3d3d342db0468b6c9e5cdd1c708e9624ac2
Enzo Diaz <enzodiazdev@gmail.com>
2020-03-31 15:05:39 -0300

    temporary dev comment

--- src/utils/translate.js
+++ src/utils/translate.js
@@ -1,14 +1,20 @@
-const options = { }
-const googleTranslate = require('google-translate')(process.env.GOOGLE_TRANSLATE_API_KEY, options)
+// const options = { }
+// const googleTranslate = require('google-translate')(process.env.GOOGLE_TRANSLATE_API_KEY, options)
 
 
-export default async function translate ({ text, srcLang, targetLang }) {
-	return new Promise(function (resolve, reject) {
-		googleTranslate.translate(text, srcLang, targetLang, function(err, translation) {
-			if (err)
-				return reject(err)
+// export default async function translate ({ text, srcLang, targetLang }) {
+// 	return new Promise(function (resolve, reject) {
+// 		googleTranslate.translate(text, srcLang, targetLang, function(err, translation) {
+// 			if (err)
+// 				return reject(err)
 
-		  resolve(translation)
-		})
-	})
+// 		  resolve(translation)
+// 		})
+// 	})
+// }
+//
+//DEV NOTE: commented to prevent api error. 
+
+export default async function translate (){
+	return new Promise((res, rej) => res({translatedText: "traducido"}))
 }
